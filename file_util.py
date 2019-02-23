916056fe41103cedf1127388cb925c8769bc9720
xususan <susan.xu97@gmail.com>
2019-02-23 14:48:45 -0500

    additional utility files, add script to get header, use only columns defined in config

---
+++ file_util.py
@@ -0,0 +1,25 @@
+#!/usr/bin/evn python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2019-02-18
+@author waldo
+"""
+
+
+def read_int_config_file(file_name):
+    """
+    Read a configuration file that is a csv where the first entry in each line is an integer
+    If the first element in a line contains something other than
+    an integer, it will simply be skipped. Returns a list of integers in ascending order
+    :param file_name: the name of the configuration file
+    :return: a sorted list of integers that were in the file
+    """
+    fin = open(file_name, 'r')
+    ret_l = []
+    for l in fin:
+        try:
+            ret_l.append(int(l.split(',')[0]))
+        except:
+            continue
+    fin.close()
+    return sorted(ret_l)
--- get_counts.py
+++ get_counts.py
@@ -5,16 +5,18 @@ Created on 2019-02-18
 @author Susan
 Get the number of unique values in each column of a dataset.
 """
-import random, sys, csv
+import random, sys, csv, file_util
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('Usage: python get_counts.py file')
+    if len(sys.argv) < 3:
+        print('Usage: python get_counts.py file config_file')
         sys.exit(1)
 
     fin = open(sys.argv[1], 'r')
     csv_in = csv.reader(fin)
     header = next(csv_in)
+
+    columns = set(file_util.read_int_config_file(sys.argv[2]))
 
     unique_values = {}
     for col in range(len(header)):
@@ -22,14 +24,14 @@ if __name__ == '__main__':
 
     for line in csv_in:
         for index, item in enumerate(line):
-            if item in unique_values[index]:
-                unique_values[index][item] += 1
-            else:
-                unique_values[index][item] = 1
+            if index in columns:
+                if item in unique_values[index]:
+                    unique_values[index][item] += 1
+                else:
+                    unique_values[index][item] = 1
 
     for i, col in enumerate(header):
-        if len(unique_values[i]) < 20:
-            print(col, unique_values[i])
+        print(col, unique_values[i])
 
 
     fin.close()
