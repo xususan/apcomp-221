bc6cfc3021e2671d9d5c3dc260a60ead69bbc0be
xususan <susan.xu97@gmail.com>
2019-03-30 13:21:45 -0400

    remove duplicates

---
+++ pset2/src/clean_csv.py
@@ -0,0 +1,40 @@
+#!/usr/bin/evn python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2019-03-30
+@author samuelclay
+
+Removes
+
+"""
+import sys
+import pdb
+from collections import defaultdict, Counter
+from file_util import columns_from_config_file, read_csv, write_csv_to_file
+    
+    
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Usage: python clean_csv.py infile.csv outfile.csv')
+        sys.exit(1)
+    
+    headers, rows = read_csv(filename=sys.argv[1])
+    out_filename = sys.argv[2]
+
+    output_csv = [[]]
+    for header in headers:
+        output_csv[0].append(header)
+
+    counter = Counter()
+    for line in rows:
+        identifiers = ','.join(line[:2])
+        counter.update(identifiers)
+        if counter[identifiers] == 1:
+        	output_csv.append(line)
+
+    write_csv_to_file(output_csv, out_filename)
+
+
+
+
+
