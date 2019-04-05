36b9a65e8666b847f7067a98a9b58571fa111ae1
Samuel Clay <samuel@ofbrooklyn.com>
2019-04-05 15:40:46 -0400

    Accidentally deleted code.

--- pset2/src/clean_csv.py
+++ pset2/src/clean_csv.py
@@ -15,7 +15,9 @@ def deduplicate(rows, headers):
     """
     Given a CSV reader, deduplicates values based on identifiers.
 
-    Returns a list of rows with entries.
+    :param rows: rows of column arrays
+    :param headers: row with column names
+    :return: a list to write to CSV with deduplicated entries.
     """
     
     # Move 'completed' header to the end because that's where the data is
@@ -43,8 +45,6 @@ def deduplicate(rows, headers):
         if counter[identifiers] == 1:
             deduplicated_values[identifiers] = line   
 
-        # print(deduplicated_values)
-
         if counter[identifiers]:
             if (len(deduplicated_values[identifiers]) != n_cols and len(line) == n_cols) or (
                 len(deduplicated_values[identifiers]) < len(line) and len(line) <= n_cols) or (
@@ -59,6 +59,32 @@ def deduplicate(rows, headers):
 
     return output_csv
 
+def remove_rows_with_missing(output_csv):
+    """
+    Split CSV into a corrupt and non-corrupt CSV based on number of columns.
+
+    :param output_csv: Iterator over header and rows of a CSV
+    :return: tuple, where the first entry is the non-corrupt CSVs and the second entry is the corrupt CSV
+    """
+    headers = output_csv[0]
+    rows = output_csv[1:]
+    n_cols = len(headers)
+
+    corrupt_csv = []
+    corrupt_csv.append(headers)
+
+    non_corrupt_csv = []
+    non_corrupt_csv.append(headers)
+
+    for line in rows:
+        if len(line) != n_cols:
+            corrupt_csv.append(line)
+        else:
+            non_corrupt_csv.append(line)
+
+    return non_corrupt_csv, corrupt_csv
+
+
     
 if __name__ == '__main__':
     if len(sys.argv) < 3:
@@ -66,12 +92,10 @@ if __name__ == '__main__':
         sys.exit(1)
     
     headers, rows = read_csv(filename=sys.argv[1])
-    out_filename = sys.argv[2]
+    out_non_corrupt_filename = sys.argv[2]
+    out_corrupt_filename = out_non_corrupt_filename[:-4] + '_corrupt.csv'
 
     output_csv = deduplicate(rows, headers)
-    write_csv_to_file(output_csv, out_filename)
-
-
-
-
-
+    non_corrupt_csv, corrupt_csv = remove_rows_with_missing(output_csv)
+    write_csv_to_file(non_corrupt_csv, out_non_corrupt_filename)
+    write_csv_to_file(corrupt_csv, out_corrupt_filename)
