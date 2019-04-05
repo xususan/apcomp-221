1478771de602668323c3a724045978c6ec0d3fda
xususan <susan.xu97@gmail.com>
2019-04-05 13:47:37 -0400

    add tests and refactor clean_csv

--- pset2/src/clean_csv.py
+++ pset2/src/clean_csv.py
@@ -2,29 +2,28 @@
 # -*- coding: utf-8 -*-
 """
 Created on 2019-03-30
-@author samuelclay
+@author xususan
 
-Removes
+Removes duplicates from CSV and writes to file.
 
 """
 import pdb
 import sys
 from collections import defaultdict, Counter
 from file_util import read_csv, write_csv_to_file
-    
-    
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print('Usage: python clean_csv.py infile.csv outfile.csv')
-        sys.exit(1)
-    
-    headers, rows = read_csv(filename=sys.argv[1])
-    out_filename = sys.argv[2]
+   
+def deduplicate(rows, headers): 
+    """
+    Given a CSV reader, deduplicates values based on identifiers.
 
+    Returns a list of rows with entries.
+    """
     # Write headers to file
     output_csv = [[]]
     for header in headers:
         output_csv[0].append(header)
+
+    n_cols = len(headers)
 
 
     deduplicated_values = {}
@@ -39,13 +38,29 @@ if __name__ == '__main__':
         if counter[identifiers] == 1:
             deduplicated_values[identifiers] = line   
 
-        if counter[identifiers] > 1 and len(deduplicated_values[identifiers]) != 48 and len(line) == 48:
-            deduplicated_values[identifiers] = line
+        # print(deduplicated_values)
+
+        if counter[identifiers]:
+            if (len(deduplicated_values[identifiers]) != n_cols and len(line) == n_cols) or (
+                len(deduplicated_values[identifiers]) < len(line) and len(line) <= n_cols) or (
+                len(deduplicated_values[identifiers]) > n_cols):
+                deduplicated_values[identifiers] = line
 
     for line in deduplicated_values.values():
         output_csv.append(line)
 
+    return output_csv
 
+    
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Usage: python clean_csv.py infile.csv outfile.csv')
+        sys.exit(1)
+    
+    headers, rows = read_csv(filename=sys.argv[1])
+    out_filename = sys.argv[2]
+
+    output_csv = deduplicate(rows, headers)
     write_csv_to_file(output_csv, out_filename)
 
 
